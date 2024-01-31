34b9f4e909cced09de2b059587bb332d1d172d3d
franklindyer <franklin@dyer.me>
2024-01-30 22:27:21 -0700

    Completed the following: - finished all of the AgoraDatabaseManager functions (I think!) - modified some of the notes/docs - created a rudimentary test for AgoraDatabaseManager in /src/tests

--- notes.md
+++ notes.md
@@ -8,12 +8,19 @@ getRecovery        : RO get user with given recovery code, if any
 userExists         : RO check if user ID exists
 postExists         : RO check if post ID exists
 imgExists          : RO check is img ID exists
+tokenExists        : RO check if a token is valid and of a given type
 
 getPublicUser      : RO get public info of user by ID (name, pfp, friends, likes, posts)
+isUserSuspended    : RO determine if user is suspended
+isUserConfirmed    : RO determine is user is confirmed
+isAdmin            : RO determine if user is admin
 getPrivateUser     : RO get private info of user by ID (public info + email, images)
 getPostInfo        : RO get post title and filename (to be replaced with content) as well as likes/dislikes and comments
 searchUser         : RO get list of users with usernames like query
 searchPost         : RO get list of posts with title like query
+
+createUser         : RW create user with given username, email, password, recovery code
+verifyUser         : RW change user to "confirmed"
 
 createToken        : RW create token of specific type for user
 expireToken        : RW delete token of specific type for user
@@ -43,37 +50,23 @@ Some notes about user actions:
 
 ```
 createAccount   : EmailAddr -> Username -> Password -> CreateAccountToken
-    POST to /createaccount with attributes "email", "username", "password"
 confirmCreate   : CreateAccountToken -> BackupCode
-    GET to /confirmaccount with query string argument "token"
 
 login           : Username -> Password -> SessionToken
-    POST to /login with attributes "username", "password"
 logout          : SessionToken -> ()
-    POST to /logout with cookie "session"
 
 deleteAccount   : SessionToken -> Password -> DeleteAccountToken
-    POST to /deleteaccount with cookie "session" and attribute "password"
 comfirmDelete   : DeleteAccountToken -> ()
-    GET to /confirmdelete with query string argument "token"
 
 recoverAccount  : EmailAddr -> RecoveryToken
-    POST to /recover with attribute "email"
 backupRecover   : BackupCode -> EmailAddr -> RecoveryToken
-    POST to /backup with attributes "code" and "email"
 confirmRecover  : RecoveryToken -> Password -> ()
-    POST to /confirmrecover with attributes "token" and "password"
 
 getUser         : UserID -> UserPublicInfo
-    GET to /profile/<uid>
 getPost         : PostID -> PostInfo
-    GET to /post/<pid>
 getImage        : ImageID -> Image 
-    GET to /img/<img_id>
 searchUsers     : String -> [UserSummaryInfo]
-    GET to /users with query string argument "query"
 searchPosts     : String -> [PostSummaryInfo]
-    GET to /posts with query string argument "query"
 
 getMyUser       : SessionToken -> UserPrivateInfo
 
@@ -104,19 +97,3 @@ adminUnsuspend  : SessionToken -> UserID -> ()
 adminDelete     : SessionToken -> UserID -> Password -> ()
 ```
 
-API responses will take the form of JSON. An error message will take this form:
-```
-{
-    "success": 0,
-    "message": "some error message goes here"
-}
-```
-and a successful response will take this form:
-```
-{
-    "success": 1,
-    "data": {
-        ...
-    }
-}
-```
--- src/utilities/AgoraDatabaseManager.py
+++ src/utilities/AgoraDatabaseManager.py
@@ -15,11 +15,19 @@ class AgoraDatabaseManager:
     def cur(self):
         return self.conn.cursor()
 
+    def commit(self):
+        self.conn.commit()
+
     def query(self, query, args=(), one=False):
         cur = self.cur().execute(query, args)
         res = [x for x in cur.fetchall()]
         cur.close()
         return (res if len(res) > 0 else None)
+
+    def execute(self, query, args=(), one=False):
+        cur = self.cur().execute(query, args)
+        cur.close()
+        self.commit()
 
 
 
@@ -53,6 +61,10 @@ class AgoraDatabaseManager:
         res = self.query("SELECT imgid FROM images WHERE accessid = ?", (imgId))
         return (None if res is None else res[0]['imgid'])
 
+    def tokenExists(self, token, type):
+        res = self.query("SELECT owner FROM tokens WHERE value = ? AND type = ?", (token, type,))
+        return (None if res is None else res[0]['owner'])
+
 
 
     def getPublicUser(self, uid):
@@ -60,17 +72,35 @@ class AgoraDatabaseManager:
         info = res[0]
         res = self.query("SELECT pid, title FROM posts WHERE owner = ?", (uid,))
         info["posts"] = [] if res is None else [post for post in res]
-        res = self.query("SELECT user1, user2 FROM friendships WHERE (user1 = ? OR user2 = ?) AND accept1 = 1 AND accept2 = 1", (uid, uid,))
-        info["friends"] = [] if res is None else [tup['user1'] if tup[0] != uid else tup['user2'] for tup in res] 
+        res = self.query("SELECT user1, user2 FROM friendships WHERE (user1 = ? OR user2 = ?) AND accepted = 1", (uid, uid,))
+        info["friends"] = [] if res is None else [tup['user1'] if tup['user1'] != uid else tup['user2'] for tup in res] 
         return info
+
+    def isUserSuspended(self, uid):
+        res = self.query("SELECT suspended FROM users WHERE uid = ?", (uid,))
+        return (res[0]['suspended'] == 1)
+    
+    def isUserConfirmed(self, uid):
+        res = self.query("SELECT confirmed FROM users WHERE uid = ?", (uid,))
+        return (res[0]['confirmed'] == 1)
+
+    def isUserAdmin(self, uid):
+        res = self.query("SELECT admin FROM users WHERE uid = ?", (uid,))
+        return (res[0]['admin'] == 1)
 
     def getPrivateUser(self, uid):
         res = self.query("SELECT uid, username, email, pfp, status, suspended, admin FROM users WHERE uid = ?", (uid,))
         info = res[0]
         res = self.query("SELECT pid, title FROM posts WHERE owner = ?", (uid,))
         info["posts"] = [] if res is None else [post for post in res]
-        res = self.query("SELECT user1, user2 FROM friendships WHERE (user1 = ? OR user2 = ?) AND accept1 = 1 AND accept2 = 1", (uid, uid,))
-        info["friends"] = [] if res is None else [tup['user1'] if tup['user1'] != uid else tup['user2'] for tup in res] 
+        
+        res = self.query("SELECT user1, user2 FROM friendships WHERE (user1 = ? OR user2 = ?) AND accepted = 1", (uid, uid,))
+        info["friends"] = [] if res is None else [tup['user1'] if tup['user1'] != uid else tup['user2'] for tup in res]
+        res = self.query("SELECT user2 FROM friendships WHERE user1 = ? AND accepted = 0", (uid,))
+        info["fromyou"] = [] if res is None else [r['user2'] for r in res]
+        res = self.query("SELECT user1 FROM friendships WHERE user2 = ? AND accepted = 0", (uid,))
+        info["foryou"] = [] if res is None else [r['user1'] for r in res]
+
         res = self.query("SELECT accessid, title FROM images WHERE owner = ?", (uid,))
         info["images"] = [] if res is None else [img for img in res]
         return info
@@ -93,3 +123,85 @@ class AgoraDatabaseManager:
     def searchPost(self, substr):
         res = self.query("SELECT pid, title FROM posts WHERE title LIKE CONCAT('%', ?, '%') ORDER BY timestamp DESC", (substr,))
         return (None if res is None else [r for r in res])
+
+
+
+    def createUser(self, email, username, hpassword, hrecovery):
+        self.execute("INSERT INTO users (email, username, hpassword, hrecovery) VALUES (?, ?, ?, ?)", (email, username, hpassword, hrecovery,))
+
+    def verifyUser(self, uid):
+        self.execute("UPDATE users SET confirmed = 1 WHERE uid = ?", (uid,))
+
+
+
+    def createToken(self, uid, token, type):
+        self.execute("INSERT INTO tokens (owner, value, type) VALUES (?, ?, ?)", (uid, token, type))
+
+    def expireToken(self, token):
+        self.execute("DELETE FROM tokens WHERE value = ?", (token,))
+
+
+
+    def setStatus(self, uid, status):
+        self.execute("UPDATE users SET status = ? WHERE uid = ?", (status, uid,))
+
+    def setPicture(self, uid, imgid):
+        self.execute("UPDATE users SET pfp = ? WHERE uid = ?", (imgid, uid,))
+
+    def setEmail(self, uid, email):
+        self.execute("UPDATE users SET email = ? WHERE uid = ?", (email, uid,))
+
+    def setUsername(self, uid, username):
+        self.execute("UPDATE users SET username = ? WHERE uid = ?", (username, uid,))
+
+
+
+    def insertPost(self, uid, title, location):
+        self.execute("INSERT INTO posts (owner, title, filename) VALUES (?, ?, ?)", (uid, title, location,))
+
+    def insertImage(self, uid, title, location, accessid):
+        self.execute("INSERT INTO images (owner, title, filename, accessid) VALUES (?, ?, ?, ?)", (uid, title, location, accessid,))
+
+    def insertComment(self, uid, pid, comment):
+        self.execute("INSERT INTO comments (owner, post, content) VALUES (?, ?, ?)", (uid, pid, comment,))
+
+
+
+    def insertFriendReq(self, uid1, uid2):
+        self.execute("UPDATE friendships SET accepted = 1 WHERE user1 = ? AND user2 = ?", (uid2, uid1,))
+        res = self.query("SELECT accepted FROM friendships WHERE user1 = ? AND user2 = ?", (uid1, uid2,))
+        if res is None:
+            self.execute("INSERT INTO friendships (user1, user2) VALUES (?, ?)", (uid1, uid2,))
+
+    def confirmFriendReq(self, uid1, uid2):
+        self.insertFriendReq(uid1, uid2)
+
+    def deleteFriendReq(self, uid1, uid2):
+        self.execute("DELETE FROM friendships WHERE (user1 = ? AND user2 = ?) OR (user2 = ? AND user1 = ?)", (uid1, uid2, uid1, uid2,))
+
+
+
+    def deletePost(self, pid):
+        self.execute("DELETE FROM posts WHERE pid = ?", (pid,))
+
+    def deleteImage(self, accessid):
+        self.execute("DELETE FROM images WHERE accessid = ?", (accessid,))
+
+    def deleteUser(self, uid):
+        self.execute("DELETE FROM users WHERE uid = ?", (uid,))
+        self.execute("DELETE FROM tokens WHERE owner = ?", (uid,))
+        self.execute("DELETE FROM posts WHERE owner = ?", (uid,))
+        self.execute("DELETE FROM comments WHERE owner = ?", (uid,))
+        self.execute("DELETE FROM images WHERE owner = ?", (uid,))
+        self.execute("DELETE FROM reports WHERE owner = ?", (uid,))
+        self.execute("DELETE FROM friendships WHERE user1 = ? OR user2 = ?", (uid,))
+        self.execute("DELETE FROM votes WHERE owner = ?", (uid,))
+
+
+
+    def suspendUser(self, uid):
+        self.execute("UPDATE users SET suspended = 1 WHERE uid = ?", (uid,))
+
+    def unsuspendUser(self, uid):
+        self.execute("UPDATE users SET suspended = 0 WHERE uid = ?", (uid,))
+
