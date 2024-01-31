057448d3ad460d595b512f7bc556c777fb8786d5
Franklin <franklin@dyer.me>
2024-01-31 08:01:24 +0000

    added dockerfile and some server paths for testing

--- src/server.py
+++ src/server.py
@@ -7,11 +7,26 @@ sys.path.insert(1, './utilities')
 from limits import *
 from agora_errors import *
 from AgoraSyntacticFilter import *
+from AgoraSemanticFilter import *
+from AgoraFilter import *
+from AgoraDatabaseManager import *
+
+agoraInterpreter = new AgoraFilter(None)
+agoraSemantics = new AgoraSemanticFilter(agoraInterpreter)
+agoraSyntax = new AgoraSyntacticFiter(agoraSemantics)
 
 app = Flask(__name__)
+
+@app.errorhandler(500)
+def error500():
+    return "Something went wrong server-side. Oopsie!"
 
 @app.route('/')
 def home():
     return "Home page!"
 
-app.run()
+@app.route('/users')
+def users():
+    return "Coming soon..."
+
+app.run(host = "0.0.0.0", port = sys.argv[1])
